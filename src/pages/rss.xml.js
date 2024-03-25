a213afb7b22aa4bf95ad780308069df77b1473b1
luismateoh <luismateohm@gmail.com>
2024-03-25 12:32:20 -0500

    Update rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { siteConfig } from '@/config/site';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection('events');
 	return rss({
 		title: siteConfig.name,
 		description: siteConfig.description,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/events/${post.slug}/`,
 		})),
 	});
 }
