42731b8c27912eb1853be7c28d458ca983e863b5
luismateoh <luismateohm@gmail.com>
2024-03-27 01:24:11 -0500

    update carrer list

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-nomy.vercel.app",
+  site: "https://aldebaran-eta.vercel.app/",
   integrations: [
     mdx({
       syntaxHighlight: "shiki",
