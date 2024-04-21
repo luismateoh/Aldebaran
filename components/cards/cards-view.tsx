06afac577e6f92851fcfc27afe01f142994c0f37
luismateoh <luismateohm@gmail.com>
2024-04-20 23:23:53 -0500

    Added button see more events

--- components/cards/cards-view.tsx
+++ components/cards/cards-view.tsx
@@ -1,9 +1,15 @@
-import { getAllEventsIds, getSortedEventsData } from "@/lib/events"
+import {
+  getAllEventsByMonth,
+  getAllEventsIds,
+  getSortedEventsData,
+} from "@/lib/events"
 import EventsListCards from "@/components/cards/events-list-cards"
 
 export default async function CardsView() {
   const eventsData = getSortedEventsData()
   const eventsIds = getAllEventsIds()
+  const eventsByMonth = getAllEventsByMonth()
+  console.log(eventsByMonth)
 
   return (
     <div>
